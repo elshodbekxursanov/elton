46121b6e86802145117480cd5043742b671b77e0
unknown <avazlapasov198@gmail.com>
2025-10-09 14:01:51 +0500

    yangi reja

--- money.py
+++ money.py
@@ -1,19 +1,87 @@
-student={
-    'aziz':{'matematika':5,'ingliz tili':3},
-    'azam':{'matematika':4,'ingliz tili':4},
-    'vali':{'matematika':2,'ingliz tili':3.5},
-    'ali':{'matematika':5,'ingliz tili':2},
-}
-student.update({'azamat':{'matematika':3,'ingliz tili':3}})
-student.pop('aziz')
-print(student)
-for ism,fanlar in student.items():
-    print(ism,fanlar)
-    ortacha=sum(fanlar.values())/len(fanlar)
-    if ortacha<3:
-        print(f"{ism.title()}-siz uta olmadingiz:")
-    else:
-        print(f"{ism.title()}-tabriklayman uta oldingiz")
-        .
-ssh-keygen -t ed25519 -C "avazlapasov198@gmail.com"
-"avazlapasov198@gmail.com"
+# def hello(ism):
+#     print(f'assalomu aleykum {ism}')
+# hello(input("ism kiriting:"))
+
+# def kvadrat(son):
+#     print(son*son)
+# kvadrat(int(input("sonni kiritng:")))
+
+# def yosh(age):
+#     year=2025
+#     print(f"{year}-{age}={year-age}")
+# yosh(int(input("yilingizni kiritng:")))
+
+# son=[2,4,3,5,7]
+# def kvadrat(number):
+#     for n in son:
+#         print(n**2)
+# kvadrat(5)
+#
+# def open_close():
+#     kod=12345678
+#     def check_parol(parol):
+#         return parol==kod
+#     return check_parol
+# sana=0
+# while True:
+#     user=int(input('parolni kiriting:'))
+#     if user==0:
+#         break
+#     sana+=1
+#     answer=open_close()
+#     n=answer(user)
+#     if n:
+#         print('siz kirishingiz mumkin:')
+#         break
+#     else:
+#         print('xato')
+#     if sana==3:
+#         print("yuqolllllll")
+#         break
+
+# def savdo():
+#     mahsulotlar={
+#         'olma':5000,
+#         'anor':12000,
+#         'behi':21000,
+#         'olcha':10000
+#     }
+#     for meva,narx in mahsulotlar.items():
+#         print(meva,narx)
+#     jami=sum(mahsulotlar.values())
+#     print(f"jami summa {jami} buldi")
+#     if jami>30000:
+#         print(jami-jami/100)
+# savdo()
+
+# def email_tekshir():
+#     email = input("Email manzilingizni kiriting: ")
+#
+#     if "@" in email and "." in email:
+#         print(" Email manzili to‘g‘ri.")
+#     else:
+#         print(" Email manzili noto‘g‘ri.")
+#
+# email_tekshir()
+# son=[1,2,3,4,5,6,7,8,9,10,11,1,2]
+# def juft():
+#     for i in son:
+#         if i%2==0:
+#             print(i)
+# juft()
+#
+# def oylikni_hisoblash():
+#     soat=float(input('ishlagan soatingizni kiriting:'))
+#     stavka=float(input(' soatlik stavkani kiriting:'))
+#     oylik=soat*stavka
+#     print(f"ishchining oyligi {oylik}$")
+# oylikni_hisoblash()
+
+
+
+
+
+
+
+
+
